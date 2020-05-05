e12b6134ac675f7b0ebc5770d5573f20e1511e0a
paul <npn.perm@gmail.com>
2020-05-05 18:58:12 +0500

    Добавил проверку на null и пустоту входной строки

--- jjm/src/com/company/Strings_1.java
+++ jjm/src/com/company/Strings_1.java
@@ -5,21 +5,23 @@ public class Strings_1 {
 
     public static void main(String[] args) {
         String data = "При*вет*, мой дорог*ой друг! К*ак тв*ои д*ела?";
+//        String data = "";
         replace(data);
     }
 
     // удаляет указанный символ * и выводит в консоль без него
     private static void replace(String data) {
-
-// можно и так, с использованием стандартных методов
-//        System.out.println(data.replace("*",""));
-        String newData = ""; // в эту строку будет записываться новая строка
-        char ch = '\u002A'; // искомый символ для замены - *
-        for (int i = 0; i < data.length(); i++) { // проверяем каждый символ в строке
-            if (data.charAt(i) != ch) { // если он не соответствует искомому
-                newData += data.charAt(i); // добавляем его в новую строку
+        if (data == null || data.length() == 0) { // проверка на null и на пустоту
+            System.out.println("Empty data"); // если да - сообщение об ошибке
+        } else { // иначе - все ок, выполняем программу
+            String newData = ""; // в эту строку будет записываться новая строка
+            char ch = '\u002A'; // искомый символ для замены - *
+            for (int i = 0; i < data.length(); i++) { // проверяем каждый символ в строке
+                if (data.charAt(i) != ch) { // если он не соответствует искомому
+                    newData += data.charAt(i); // добавляем его в новую строку
+                }
             }
+            System.out.println(newData); // выводим в консоль новую строку
         }
-        System.out.println(newData); // выводим в консоль новую строку
     }
 }

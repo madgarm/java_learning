979a65d01c608f8eddfc84db8925ea62ae763659
paul <npn.perm@gmail.com>
2020-05-08 23:05:20 +0500

    Исправил все. Теперь возвращает только класс объекта, подходящий дял хранения данных, передаваемых в метод

--- jjm/src/com/company/Wrapper_classes_1.java
+++ jjm/src/com/company/Wrapper_classes_1.java
@@ -2,37 +2,27 @@ package com.company;
 
 public class Wrapper_classes_1 {
     public static void main(String[] args) {
-        Long one = 1200008L;
+        Long one = 1223452L;
         TypeSelector qwe = new TypeSelector();
         System.out.println(qwe.selectClass(one));
     }
 
     public static class TypeSelector {
 
-        public static Object selectClass(Long instance) {
+        public static Class selectClass(Long instance) {
             if (instance == null) {
-                Object result = instance;
-                return result;
+                return Object.class;
             } else {
                 if (instance >= -Math.pow(2, 7) && instance <= Math.pow(2, 7) - 1) {
-                    Byte result = instance.byteValue();
-                    System.out.println(result.getClass());
-                    return result;
+                    return Byte.class;
                 } else if (instance >= -Math.pow(2, 15) && instance <= Math.pow(2, 15) - 1) {
-                    Short result = instance.shortValue();
-                    System.out.println(result.getClass());
-                    return result;
+                    return Short.class;
                 } else if (instance >= -Math.pow(2, 32) && instance <= Math.pow(2, 32) - 1) {
-                    Integer result = instance.intValue();
-                    System.out.println(result.getClass());
-                    return result;
+                    return Integer.class;
                 } else if (instance >= -Math.pow(2, 63) && instance <= Math.pow(2, 63) - 1) {
-                    Long result = instance.longValue();
-                    System.out.println(result.getClass());
-                    return result;
+                    return Long.class;
                 } else {
-                    Object result = instance;
-                    return result;
+                    return Object.class;
                 }
             }
         }

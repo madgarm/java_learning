7979160b36afa847665fff18d06b9774e99876f0
paul <npn.perm@gmail.com>
2020-05-14 22:14:35 +0500

    Поправил по замечаниям

--- jjm/src/com/intellekta/creditbank/CreditCalculator.java
+++ jjm/src/com/intellekta/creditbank/CreditCalculator.java
@@ -4,38 +4,58 @@ import java.util.Scanner;
 
 public class CreditCalculator {
 
-    String lastName, firstName, secondName; // объявляем поля имя, фамилия, отчество
+    String lastName, firstName, secondName; // объявляем поля фамилия, имя, отчество
     double sum, interestRate; // объявляем поля суммы кредита, годовой ставки
-    int loanMaturity; // объявляем полесрока кредита в месяцах
+    int loanMaturity; // объявляем поле срока кредита в месяцах
 
-    public CreditCalculator() { // констуруктор класса без параметров
+    public CreditCalculator() { // конструктор класса без параметров
         Scanner scanner = new Scanner(System.in); // создаем сканер
 
-        // присваиваем параметр имя
+        // присваиваем параметр фамилия
         do {
-            System.out.print("Lastname: "); // предлагаем ввести имя
+            System.out.print("Lastname: "); // предлагаем ввести фамилию
+            while (!scanner.hasNextLine()) {
+                System.out.println("Lastname is incorrect. It must be not empty string"); // сообщение об ошибке
+                System.out.print("Lastname: "); // предлагаем ввести фамилию
+                scanner.next();
+            }
             this.lastName = scanner.nextLine(); // читаем строку из ввода
             if (this.lastName.intern() == "") { // проверяем на пустоту
                 System.out.println("Lastname is incorrect. It must be not empty string"); // сообщение об ошибке
             }
         } while (this.lastName.intern() == ""); // добиваемся нормального ввода
 
-        // присваиваем параметр фамилия
+        // присваиваем параметр имя
         do {
-            System.out.print("Firstname: "); // предлагаем ввести фамилию
+            System.out.print("Firstname: "); // предлагаем ввести имя
+            while (!scanner.hasNextLine()) {
+                System.out.println("Firstname is incorrect. It must be not empty string"); // сообщение об ошибке
+                System.out.print("Firstname: "); // предлагаем ввести имя
+                scanner.next();
+            }
             this.firstName = scanner.nextLine(); // читаем строку из ввода
             if (this.firstName.intern() == "") { // проверяем на пустоту
                 System.out.println("Firstname is incorrect. It must be not empty string"); // сообщение об ошибке
             }
         } while (this.firstName.intern() == ""); // добиваемся нормального ввода
 
-        // присваиваем параметр отчество, не важно, что там вводят
+        // присваиваем параметр отчество
         System.out.print("Secondname: "); // предлагаем ввести отчество
+        while (!scanner.hasNextLine()) {
+            System.out.println("Error"); // сообщение об ошибке
+            System.out.print("Secondname: "); // предлагаем ввести отчество
+            scanner.next();
+        }
         this.secondName = scanner.nextLine(); // читаем строку из ввода
 
         // присваиваем параметр процентной ставки
         do {
             System.out.print("Interest rate: "); // предлагаем ввести процентную ставку
+            while (!scanner.hasNextDouble()) {
+                System.out.println("Interest rate is incorrect. It must be strictly positive number"); // сообщение об ошибке
+                System.out.print("Interest rate: "); // предлагаем ввести процентную ставку
+                scanner.next();
+            }
             this.interestRate = scanner.nextDouble(); // читаем действительное число из ввода
             if (this.interestRate < 0.0) { // проверяем на положительность
                 System.out.println("Interest rate is incorrect. It must be strictly positive number"); // сообщение об ошибке
@@ -45,6 +65,11 @@ public class CreditCalculator {
         // присваиваем параметр суммы кредита
         do {
             System.out.print("Credit amount: "); // предлагаем ввести сумму кредита
+            while (!scanner.hasNextDouble()) {
+                System.out.println("Credit amount is incorrect. It must be strictly positive number"); // сообщение об ошибке
+                System.out.print("Credit amount: "); // предлагаем ввести сумму кредита
+                scanner.next();
+            }
             this.sum = scanner.nextDouble(); // читаем действительное число из ввода
             if (this.sum < 0.0) { // проверяем на положительность
                 System.out.println("Credit amount is incorrect. It must be strictly positive number"); // сообщение об ошибке
@@ -52,9 +77,15 @@ public class CreditCalculator {
         } while (this.sum < 0.0); // добиваемся нормального ввода
 
         // присваиваем параметр срока кредита
-        double inputLoanMaturity; // переменная для временного хранения вводаимого числа
+
+        double inputLoanMaturity; // переменная для временного хранения вводимого числа
         do {
             System.out.print("Loan maturity: "); // предлагаем ввести срок кредита
+            while (!scanner.hasNextDouble()) {
+                System.out.println("Loan maturity is incorrect. It must be strictly positive int"); // сообщение об ошибке
+                System.out.print("Loan maturity: "); // предлагаем ввести срок кредита
+                scanner.next();
+            }
             inputLoanMaturity = scanner.nextDouble(); // читаем действительное число из ввода
             if (inputLoanMaturity % 1 != 0.0 || inputLoanMaturity < 0) { // проверяем на целочисленность и положительность
                 System.out.println("Loan maturity is incorrect. It must be strictly positive int"); // сообщение об ошибке

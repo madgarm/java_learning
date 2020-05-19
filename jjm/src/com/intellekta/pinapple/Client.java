697ed3d4ee7fb2edcb0b6770ea19ef0c11d211bc
paul <npn.perm@gmail.com>
2020-05-19 13:51:01 +0500

    Селектор поправил для type

--- jjm/src/com/intellekta/pinapple/Client.java
+++ jjm/src/com/intellekta/pinapple/Client.java
@@ -10,8 +10,14 @@ public class Client {
 
     public Client(long id, String type, String name, double sum) { // конструктор класса с 4 входными параметрами
         this.id = (id <= 100_000_000_000L) ? (100_000_000_000L) : id; // с проверкой на значение
-        this.type = (type == null || !type.equals(typeInstitution) && !type.equals(typeIndividual)) ? (typeInstitution) : type; // проверка
+        if (type == null) { // если null, то "institution"
+            this.type = typeInstitution;
+        } else if (type.equals(typeIndividual)) { // если соответствует "individual", то "individual"
+            this.type = typeIndividual;
+        } else { // во всех иных, то "institution"
+            this.type = typeInstitution;
+        }
         this.name = (name != null) ? (name) : "Default"; // проверка на null
-        this.sum = (sum < 0) ? (0) : sum; // пвоверка на положительность
+        this.sum = (sum < 0) ? (0) : sum; // проверка на положительность
     }
 }

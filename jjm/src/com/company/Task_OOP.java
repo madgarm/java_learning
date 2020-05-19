a10dad8667680e4bed0d247de4f5df6d86af9bc2
paul <npn.perm@gmail.com>
2020-05-19 20:13:09 +0500

    добавил комментарии

--- jjm/src/com/company/Task_OOP.java
+++ jjm/src/com/company/Task_OOP.java
@@ -1,17 +1,17 @@
 package com.company;
 
-import com.intellekta.tesla.Lorry;
-import com.intellekta.tesla.Motorcycle;
-import com.intellekta.tesla.PassengerCar;
+import com.intellekta.tesla.Lorry; // импортируем класс Lorry
+import com.intellekta.tesla.Motorcycle; // импортируем класс Motorcycle
+import com.intellekta.tesla.PassengerCar; // импортируем класс PassengerCar
 
 public class Task_OOP {
     public static void main(String[] args) {
-        PassengerCar car = new PassengerCar(300, "Model X", 5);
-        Lorry lorry = new Lorry(620, "Semi", 6500);
-        Motorcycle moto = new Motorcycle(170, 210);
+        PassengerCar car = new PassengerCar(300, "Model X", 5); // создаем экземпляр PassengerCar
+        Lorry lorry = new Lorry(620, "Semi", 6500); // создаем экземпляр Lorry
+        Motorcycle moto = new Motorcycle(170, 210); // создаем экземпляр Motorcycle
 
-        car.printSpecification();
-        lorry.printSpecification();
-        moto.printSpecification();
+        car.printSpecification(); // выводим характеристики
+        lorry.printSpecification(); // выводим характеристики
+        moto.printSpecification(); // выводим характеристики
     }
 }

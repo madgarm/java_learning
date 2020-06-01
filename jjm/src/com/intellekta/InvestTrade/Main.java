407828fb120ca791c668d8d91eb28814e2484d1e
paul <npn.perm@gmail.com>
2020-06-02 01:41:14 +0500

    Допилил

--- jjm/src/com/intellekta/InvestTrade/Main.java
+++ jjm/src/com/intellekta/InvestTrade/Main.java
@@ -5,13 +5,15 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        double[] times = {1111.34, 1298.9, 1298.9, 1634.7, 1500, 1633.73, 1111.34, 1634.7, 1000.47, 1890.7, 1890.7};
+//        double[] times = {1111.34, 1298.9, 1298.9, 1634.7, 1500, 1633.73, 1111.34, 1634.7, 1000.47, 1890.7, 1890.7};
+        double[] times = {1111.34, 1298.9, 1633.73, 1500.1, 1634.7, 1000.47, 1890.7};
+//        double[] times = {1633.73, 1634.7, 1890.7, 1633.73, 1634.7, 1890.7, 1000.0};
+//        double[] times = {1633.73};
+//        double[] times = {1633.73, 1634.7, 1890.7, 1633.73, 1634.7, 1890.7, 1000.0};
+//        double[] times = {1633.73, 1633.73, 1633.73, 1633.73, 1633.73, 1633.73, 1890.7, 1000.0};
         double[] result;
-        result = Task.findAllWinners(times);
-        for (int i = 0; i < result.length; i++) {
-            System.out.printf("%s ", result[i]);
-        }
-        System.out.println();
+
+
 
         double[] ex = Arrays.copyOf(times, times.length);
         for (int i = 0; i < ex.length; i++) {
@@ -19,8 +21,21 @@ public class Main {
         }
         System.out.println();
 
-        int winner = Task.peopleChoiceAward(times, Task.unikWinners);
-        System.out.println(winner);
+        Arrays.sort(ex);
+        for (int i = 0; i < ex.length; i++) {
+            System.out.printf("%s ", ex[i]);
+        }
+        System.out.println();
+
+        result = Task.findAllWinners(times);
+        for (int i = 0; i < result.length; i++) {
+            System.out.printf("%s ", result[i]);
+        }
+        System.out.println();
+
+//        int winner = Task.peopleChoiceAward(times, Task.unikWinners);
+//        int winner = Task.peopleChoiceAward(times, 3);
+//        System.out.println(winner);
     }
 
 
--- jjm/src/com/intellekta/InvestTrade/Task.java
+++ jjm/src/com/intellekta/InvestTrade/Task.java
@@ -5,37 +5,65 @@ import java.util.Arrays;
 
 public class Task {
 
-    static int unikWinners = 0; // счетчик уникальных победителей
-
-    public static double[] findAllWinners(double[] data) { // принимает массив с отработнными часами всех сотрудников
-        if (data == null || data.length == 0) { // проверка на null и пустоту
-            double[] empty = new double[0]; // пустой массив
-            return empty; // возвращаем пустой массив
+    public static double[] findAllWinners(double[] data) { // принимает массив с отработанными часами всех сотрудников
+        if (data == null || data.length == 0) { // проверки на null и пустоту
+            return new double[0]; // возвращаем пустой массив
+        } else if (data.length == 1) { // если сотрудник один
+            return data; // возвращаем только его
         } else { // если же все ок с входными данными
-            Arrays.sort(data); // сортируем по возрастанию входной массив
-            int countOfWinners = 0; // переменная для хранения числа призовых мест
-            while (countOfWinners != 3) { // целевой показатель - 3
-                // сравниваем пару соседних показателей с конца, ищем отличные значения
-                if (Array.getDouble(data, data.length - unikWinners - 1) != Array.getDouble(data, data.length - unikWinners - 2)) {
-                    countOfWinners += 1; // каждая новая пара - еще одно призовое место
-                    unikWinners += 1; // и новый призер
-                } else {
-                    unikWinners += 1; // иначе только лишь призер на уже выбранное призовое место
-                }
+            int countOfEmployees = data.length - 1; // число сотрудников
+            int countOfWinners = 0; // счетчик для победивших сотрудников
+            double[] resultsForEmployees = Arrays.copyOf(data, data.length); // массив для результатов сотрудников
+            double[] winners = new double[countOfEmployees]; // массив для победителей
+            Arrays.sort(resultsForEmployees); // сортируем по возрастанию
+
+            // цикл для перебора сотрудников, для заполнения 3 призовых мест
+            for (int countOfWinPlaces = 0; countOfWinPlaces < 3; countOfWinPlaces++) {
+                do { // внутренний цикл с постусловием
+                    // записываем в массив победителей сотрудников с победным временем
+                    Array.setDouble(winners, countOfWinners++, (Array.getDouble(resultsForEmployees, countOfEmployees)));
+                } while (Array.getDouble(resultsForEmployees, countOfEmployees) == Array.getDouble(resultsForEmployees, --countOfEmployees));
             }
-            // возвращаем срез отсортированного массива с конца длиной равной числу разных призеров
-            return Arrays.copyOfRange(data, data.length - unikWinners, data.length);
+
+            return Arrays.copyOfRange(winners, 0, countOfWinners); // возвращаем массив, состоящий только из победителей
         }
     }
 
-    public static int peopleChoiceAward(double[] data, int countOfWinners) { // тот же массив исходный и число призеров
-        if (data.length == 0) { // проверка на пустоту
+    public static int peopleChoiceAward(double[] data, int countOfWinners) {
+        // проверки массива на null и пустоту, количество победителей на адекватность
+        if (data == null || data.length == 0 || countOfWinners > data.length || countOfWinners < 0) {
             return -1; // возвращаем -1 как показатель
-        } else { // если все ок
-            Arrays.sort(data); // сортируем
-            int winnerId = Arrays.binarySearch(data, 1500.00); // пытаемся найти запись на 1500 часов
-            // если в отсортированном массиве его позиция раньше, чем позиции победителей, возвращаем его, иначе -1
-            return (winnerId < data.length - countOfWinners) ? winnerId : -1;
+        } else { // если же все ок с входными данными
+            int[] indexOfEmployees = new int[data.length]; // массив с номерами сотрудников в изначальном массиве
+            for (int i = 0; i < indexOfEmployees.length; i++) {
+                Array.setInt(indexOfEmployees, i, i); // заполняем номерами
+            }
+
+            // синхронно сортируем массив с результатами сотрудников и их номерами в изначальном массиве, пузырьком
+            for (int i = 1; i < data.length; i++) {
+                for (int j = 0; j < data.length - i; j++) {
+                    if (data[j] > data[j + 1]) { // обнаружив больший элемент
+                        double maxTime = data[j]; // записываем в максимум по результатам текущий
+                        int maxId = indexOfEmployees[j]; // записываем в максимум по индексам текущий
+                        data[j] = data[j + 1]; // в текущий по результатами пишем следующий
+                        data[j + 1] = maxTime; // в следующий по результатами пишем максимальный
+                        indexOfEmployees[j] = indexOfEmployees[j + 1]; // в текущий по индексам пишем следующий
+                        indexOfEmployees[j + 1] = maxId; // в следующий по индексам пишем максимальный
+                    }
+                }
+            }
+
+            // массив с сотрудниками, которые не стали победителями
+            double[] notWinners = Arrays.copyOfRange(data, 0, data.length - countOfWinners);
+            int winnerId = Arrays.binarySearch(notWinners, 1500); // пытаемся найти элемент со значением 1500
+            if (winnerId >= 0) { // если нашелся (>=0)
+                // для поиска первого сотрудника с таким результатом (в случае, если их несколько) проверяем условие с префиксным декрементом
+                while (notWinners[winnerId] == notWinners[--winnerId]) {
+                }
+                return indexOfEmployees[++winnerId]; // возвращаем индекс такого участника
+            } else { // если не нашлось
+                return -1; // возвращем -1 как показатель
+            }
         }
     }
 }
